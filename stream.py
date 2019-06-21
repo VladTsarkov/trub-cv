c633348755f46ce04278114f5820ad4b0734fccc
maxM <mw159159@gmail.com>
2019-06-21 18:14:57 +0500

    moved img files

---
+++ stream.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import imutils
+from imutils.video import VideoStream
+from Transform import Transform
+
+import time
+#vcap = cv.VideoCapture("rtsp://192.168.1.168:554/ch01/0")
+#vcap = cv.VideoCapture("rtsp://192.168.1.168:554/ch00")
+#vcap168 = cv.VideoCapture("rtsp://admin:admin@192.168.1.168:554/ch01/0")
+#vcap169 = cv.VideoCapture("rtsp://admin:admin@192.168.1.169:554/ch01/0")
+
+vcap168 = VideoStream("rtsp://admin:admin@192.168.1.168:554/ch01/0").start()
+vcap169 = VideoStream("rtsp://admin:admin@192.168.1.169:554/ch01/0").start()
+
+while(1):
+    #ret168,frame168 = vcap168.read()
+    #ret169,frame169 = vcap169.read()
+    frame168 = vcap168.read()
+    frame169 = vcap169.read()
+    #time.sleep(0.5)
+
+    """
+    #просмотр двух камер
+    frame168 = imutils.resize(frame168, width=400)
+    frame169 = imutils.resize(frame169, width=400)
+    cv.imshow('video1', frame168)
+    cv.imshow('video2', frame169)
+    """
+
+    #cv.imwrite("New1.jpg",frame168)
+    #cv.imwrite("New2.jpg",frame169)
+
+
+    #сшивка через файл
+    img1 = Transform(frame169, 0,324,3408,1131,3270,3588,188,4200)
+    img2 = Transform(frame168, 2844,1252,5500,1044,5599,4104,2404,3984)
+    cv.imwrite('holst.jpg',img1+img2)
+    
+
+    #cv.imshow('VIDEO', frame169)
+    #break
+    cv.waitKey(1)
