eb5205ebc4fedee93d4a78293bff210c6bec3684
maxM <mw159159@gmail.com>
2019-06-26 22:04:22 +0500

    blender stitching

--- testblend.py
+++ testblend.py
@@ -1,13 +1,25 @@
 import bge
 
-cout = bge.logic.getCurrentController()
-own = cout.owner
-if not hasattr(bge.logic. 'video'):
-    tex = bge.texture.materialID(own, 'IMin1.png')
-    
-    bge.logic.video = bge.texture.Texture(own, tex)
-    
-    bge.logic.video.source = bge.texture.Videoffmpeg('rtsp://admin:admin@192.168.1.169:554/ch01/2')
-    bge.logic.video.source.play()
-    
-bge.logic.video.refresh(True)
+
+
+scene = bge.logic.getCurrentScene()
+o1 = scene.objects["a"]
+o2 = scene.objects["b"]
+if not hasattr(bge.logic, 'video_a'):
+    #for o, im, v in [(o1, 'IMin1.png', '/home/student/trub-cv/img/out.avi'), (o2, 'IMin2.png', '/home/student/trub-cv/img/out2.avi')]:
+    tex = bge.texture.materialID(o1, 'IMin1.png')
+    bge.logic.video_a = bge.texture.Texture(o1, tex)
+    bge.logic.video_a.source = bge.texture.VideoFFmpeg('/home/student/trub-cv/img/out.avi')
+    bge.logic.video_a.source.scale = True
+    bge.logic.video_a.source.play()
+
+if not hasattr(bge.logic, 'video_b'):
+    #for o, im, v in [(o1, 'IMin1.png', '/home/student/trub-cv/img/out.avi'), (o2, 'IMin2.png', '/home/student/trub-cv/img/out2.avi')]:
+    tex_b = bge.texture.materialID(o2, 'IMin2.png')
+    bge.logic.video_b = bge.texture.Texture(o2, tex_b)
+    bge.logic.video_b.source = bge.texture.VideoFFmpeg('/home/student/trub-cv/img/out2.avi')
+    bge.logic.video_b.source.scale = True
+    bge.logic.video_b.source.play()
+
+bge.logic.video_a.refresh(True)
+bge.logic.video_b.refresh(True)
